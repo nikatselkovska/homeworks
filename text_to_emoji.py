9fb221e794833253a34d81c5db27b8fc6f29f92b
nikatselkovska <nikatysel@gmail.com>
2022-04-23 11:14:35 +0300

    text_to_emoji.py

--- text_to_emoji.py
+++ text_to_emoji.py
@@ -1,13 +1,13 @@
 # I understand this task might be performed with using lists, but dictionary is more right to use in this case
 
-#we put "complicated" designations ahead for handling them first and avoiding errors
+#we put "complicated" designations ahead for handling it first and avoiding errors
 text_emoji_dict = {'>:)': '😈', '>:(': '😠', ':)': '🙂', 'XD': '😂', ':(': '🙁'}
 
 user_input = input('Please enter some sentence using text emoji symbols: ')
 
-for text_emoji_key in text_emoji_dict:
-    index_key = user_input.find(text_emoji_key)
-    if index_key != -1:
-        user_input = user_input.replace(text_emoji_key, text_emoji_dict[text_emoji_key])
+for text_emoji, icon_emoji in text_emoji_dict.items():
+    if text_emoji not in user_input:
+        continue 
+    user_input = user_input.replace(text_emoji, icon_emoji)
 
 print(user_input)
