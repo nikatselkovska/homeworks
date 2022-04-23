0b4495071be126f9f422132af20298ecf3575f78
nikatselkovska <nikatysel@gmail.com>
2022-04-23 13:26:13 +0300

    cucumbers.py

--- cucumbers.py
+++ cucumbers.py
@@ -1,7 +1,7 @@
 DESIRED_STR = 'cucumber'
 
 cuc_input = input(f'Please enter some text using word \'{DESIRED_STR}\': ')
-index_cuc = cuc_input.find(DESIRED_STR)
+index_cuc = cuc_input.lower().find(DESIRED_STR)
 if index_cuc != -1:
     print(cuc_input[index_cuc::])
 else:
