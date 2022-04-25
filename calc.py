20109026c07078aa7b055fc1fa1a223e6ffc1fe4
nikatselkovska <nikatysel@gmail.com>
2022-04-25 18:58:32 +0300

    calc.py

--- calc.py
+++ calc.py
@@ -1,53 +1,77 @@
+from itertools import count
+
+
 print('Hello! I am ridiculous calculator.')
 INT_LINK = 'https://en.wikipedia.org/wiki/Integer'
 TEXT_ERROR = 'It is not an integer! See what integer is: ' + INT_LINK
+OPERATION_TYPES = '+, -, /, *, +++'
+entered_numbers = []
 while True:
     first_number = input('Put first integer operand please: ')
     if first_number == '':
         continue 
     try:
         first_number = int(first_number)
+        entered_numbers.append(first_number)
     except ValueError:
         print(TEXT_ERROR)
         continue
     break
 
 while True:
-    operation_type = input('Choose operation type please (only +, -, /, *): ')
+    operation_type = input(f'Choose operation type please (only {OPERATION_TYPES}): ')
     if operation_type == '':
         continue
-    elif len(operation_type) != 1 or operation_type not in '+-/*':
-        print('There are only +, -, /, *. Please input one of avalaible operations')
+    elif all((len(operation_type) != 1 or operation_type not in '+-/*', operation_type != '+++')):
+        print(f'There are only {OPERATION_TYPES}. Please input one of avalaible operations')
         continue
     break
 
+its_multiple_sum = operation_type == '+++'
+if its_multiple_sum:
+    operation_type = '+'
+
 while True:
-    second_number = input('Put second integer operand please: ')
+    second_number = input('Put one more integer operand please: ')
     if second_number == '':
-        continue
+        if its_multiple_sum:
+            break
+        else:
+            continue
     try:
         second_number = int(second_number)
+        entered_numbers.append(second_number)
     except ValueError:
         print(TEXT_ERROR)
         continue
-    break
+    if its_multiple_sum:
+        continue
+    else:
+        break
 
-result_num = None
-if operation_type == '+':
-    result_num = first_number + second_number
-elif operation_type == '-':
-    result_num = first_number - second_number
-elif operation_type == '/':
-    try:
-        result_num = first_number / second_number
-    except ZeroDivisionError as err:
-        print('There is an error:', err)
-        quit()
-elif operation_type == '*':
-    result_num = first_number * second_number
+result_num = 0
+operands_str = ''
+for entered_number in entered_numbers:
+    if entered_numbers.index(entered_number) == 0:
+        result_num = entered_number
+    else: 
+        if operation_type == '+':
+            result_num += entered_number
+        elif operation_type == '-':
+            result_num -= entered_number
+        elif operation_type == '/':
+            try:
+                result_num /= entered_number
+            except ZeroDivisionError as err:
+                print('There is an error:', err)
+                quit()
+        elif operation_type == '*':
+            result_num *= entered_number
+    operation_str = f' {operation_type} ' if entered_numbers.index(entered_number) != len(entered_numbers) - 1 else ''
+    operands_str += str(entered_number) + operation_str
 
-result_str = f'{first_number} {operation_type} {second_number} = {result_num}'
-if result_num:
+result_str = f'{operands_str} = {result_num}'
+if operands_str:
     print(result_str)
 else:
     print('There is unexpectable runtime error, we will improve it in later version...')
