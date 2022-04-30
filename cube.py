ec088638ae501abfb3a4ca9fa8d89884679de921
nikatselkovska <nikatysel@gmail.com>
2022-04-30 20:46:37 +0300

    homework

---
+++ cube.py
@@ -0,0 +1,35 @@
+import math
+from ctb import place
+
+OUR_RANGE = 5
+# cube
+for i in range(OUR_RANGE):
+	place(i, 0, 0)
+	place(0, i, 0)
+	place(0, 0, i)
+	place(0, i, OUR_RANGE-1)
+	place(i, 0, OUR_RANGE-1)
+	place(i, OUR_RANGE-1, 0)
+	place(0, OUR_RANGE-1, i)
+	place(OUR_RANGE-1, i, 0)
+	place(OUR_RANGE-1, 0, i)
+	place(OUR_RANGE-1, OUR_RANGE-1, i)
+	place(i, OUR_RANGE-1, OUR_RANGE-1)
+	place(OUR_RANGE-1, i, OUR_RANGE-1)
+
+# square
+for i in range(OUR_RANGE):
+	place(i, 0, 0)
+	place(0, i, 0)
+	place(i, OUR_RANGE - 1, 0)
+	place(OUR_RANGE - 1, i, 0)
+
+# circle
+RADIUS = 5
+BLOCKS = 40
+
+for position in range(BLOCKS):
+  radians = 2 * math.pi * position / BLOCKS
+  x = RADIUS * math.cos(radians)
+  y = RADIUS * math.sin(radians)
+  place(x, y, 0)
