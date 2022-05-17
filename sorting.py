9dd04af8992f54cea9e453541361f6cf623283d8
nikatselkovska <nikatysel@gmail.com>
2022-05-17 19:32:07 +0300

    homework

--- sorting.py
+++ sorting.py
@@ -4,8 +4,45 @@ LENGTH = 20
 LIMIT = 500
 
 
-def sort_by_value(numbers):
-    return sorted(numbers)
+def merge_sort(my_list):
+    if len(my_list) > 1:
+        mid = len(my_list) // 2
+        left = my_list[:mid]
+        right = my_list[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+
+        # Iterator for the main list
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                # The value from the left half has been used
+                my_list[k] = left[i]
+                # Move the iterator forward
+                i += 1
+            else:
+                my_list[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            my_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            my_list[k] = right[j]
+            j += 1
+            k += 1
 
 
 num_list = []
@@ -13,4 +50,5 @@ for i in range(LENGTH):
     num_list.append(random.randint(-LIMIT, LIMIT))
 
 print(num_list)
-print(sort_by_value(num_list))
+merge_sort(num_list)
+print(num_list)
