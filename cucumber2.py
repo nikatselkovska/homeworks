41e08813de67c98267e2a87edc8a68676e3fd425
nikatselkovska <nikatysel@gmail.com>
2022-05-28 15:43:37 +0300

    homework

---
+++ cucumber2.py
@@ -0,0 +1,20 @@
+def check_cucumber(cucumber):
+    assert cucumber, 'there is empty input'
+    assert cucumber.find(' ') == -1, 'there are excess spaces'
+    assert cucumber[0].isupper(), 'the first symbol should be upper'
+    assert cucumber[1:].islower(), 'the symbols after first symbol should be lower'
+    assert cucumber == 'Cucumber', 'it is something else, not a Cucumber'
+
+
+print('Hi!')
+input_cucumber = ''
+while True:
+    input_cucumber = input('Enter please the word \'Cucumber\':')
+    try:
+        check_cucumber(input_cucumber)
+    except AssertionError as error:
+        print(error)
+        continue
+    print('Thank you')
+    break
+
